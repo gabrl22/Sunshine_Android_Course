684e81846f4512ef2bfa8154abced9cc94abc804
Gabriel Cole <lgcw92@gmail.com>
2016-10-14 20:41:48 -0500

    Weather db helper

--- app/src/test/java/com/example/gabriel/app/sunshine/data/TestUtilities.java
+++ app/src/test/java/com/example/gabriel/app/sunshine/data/TestUtilities.java
@@ -8,10 +8,12 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.test.AndroidTestCase;
 
-import com.example.android.sunshine.app.utils.PollingCheck;
+import com.example.gabriel.app.sunshine.utils.PollingCheck;
 
 import java.util.Map;
 import java.util.Set;
+
+
 
 /*
     Students: These are functions and some test data to make it easier to test your database and
